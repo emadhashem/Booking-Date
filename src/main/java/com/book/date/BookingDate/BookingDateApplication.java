a966062137bf2fe339f624c52ad4b2cf86681477
emadhashem <eali32028@gmail.com>
2023-12-26 13:46:46 +0200

    init

---
+++ src/main/java/com/book/date/BookingDate/BookingDateApplication.java
@@ -0,0 +1,13 @@
+package com.book.date.BookingDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingDateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/date/BookingDate/BookingDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.date.BookingDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
